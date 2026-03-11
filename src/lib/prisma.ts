e9291e629a310890847a85aa87736ca8f4f63023
Fumiya Adachi <fumiya4adachi@gmail.com>
2026-03-12 00:34:31 +0900

    style: update page layout

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,20 @@
-// DB接続を1か所で管理するためのファイル
+import "dotenv/config"
 import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient()
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
